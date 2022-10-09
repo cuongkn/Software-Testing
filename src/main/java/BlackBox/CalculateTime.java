87654a5ae20402846a34265dfa3676346521869b
knc <91279644+cuongkn@users.noreply.github.com>
2022-10-09 23:19:05 +0700

    Calculate Time

---
+++ src/main/java/BlackBox/CalculateTime.java
@@ -0,0 +1,128 @@
+package BlackBox;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+class Time {
+    private int hour;
+    private int minute;
+    private int second;
+    private String type;
+
+    public Time() {
+    }
+
+    public Time(int hour, int minute, int second, String type) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+        this.type = type;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Time{" +
+                "hour=" + hour +
+                ", minute=" + minute +
+                ", second=" + second +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (! (o instanceof Time)) {
+            return false;
+        }
+        Time time = (Time) o;
+        return getHour() == time.getHour() && getMinute() == time.getMinute() && getSecond() == time.getSecond() && getType().equals(time.getType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getHour(), getMinute(), getSecond(), getType());
+    }
+}
+
+public class CalculateTime {
+    public Time calculate_time(Time time) {
+        Time result = new Time();
+        result.setType(time.getType());
+        result.setSecond(time.getSecond());
+
+        if (time.getHour() < 1 || time.getHour() > 12 || time.getMinute() < 0 || time.getMinute() > 59 || time.getSecond() < 0 || time.getSecond() > 59 || (!Objects.equals(time.getType(), "AM") && !Objects.equals(time.getType(), "PM"))) {
+            throw new IllegalArgumentException("Time not valid");
+        } else {
+            if (time.getHour() == 12) time.setHour(0);
+            var hour = time.getHour();
+            var minute = time.getMinute();
+            if (time.getHour() <= 9) {
+                if (time.getMinute() < 30) {
+                    result.setHour(++hour);
+                    result.setMinute(minute += 30);
+                } else {
+                    result.setHour(hour += 2);
+                    result.setMinute(minute -= 30);
+                }
+            } else if (time.getHour() == 10) {
+                if (time.getMinute() < 30) {
+                    result.setHour(++hour);
+                    result.setMinute(minute += 30);
+                } else {
+                    result.setHour(hour += 2);
+                    result.setMinute(minute -= 30);
+                    if (Objects.equals(time.getType(), "AM")) result.setType("PM");
+                    else result.setType("AM");
+                }
+            } else if (time.getHour() == 11) {
+                if (time.getMinute() < 30) {
+                    result.setHour(++hour);
+                    result.setMinute(minute += 30);
+                } else {
+                    result.setHour(1);
+                    result.setMinute(minute -= 30);
+                }
+                if (Objects.equals(time.getType(), "AM")) result.setType("PM");
+                else result.setType("AM");
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/BlackBox/CalculateTimeTest.java
@@ -0,0 +1,295 @@
+package BlackBox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculateTimeTest {
+
+    // (Strong) Boundary Value Testing
+
+    @Test
+    void calculateEndTimeOf_0_30_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(0,30,30,"AM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_1_30_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(3,0,30,"AM"),calculate.calculate_time(new Time(1,30,30,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_2_30_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(4,0,30,"AM"),calculate.calculate_time(new Time(2,30,30,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_11_30_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(1,0,30,"PM"),calculate.calculate_time(new Time(11,30,30,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_12_30_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(2,0,30,"AM"),calculate.calculate_time(new Time(12,30,30,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_13_30_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(13,30,30,"AM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_6_n1_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(6,-1,30,"AM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_6_0_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(7,30,30,"AM"),calculate.calculate_time(new Time(6,0,30,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_1_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(7,31,30,"AM"),calculate.calculate_time(new Time(6,1,30,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_58_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,28,30,"AM"),calculate.calculate_time(new Time(6,58,30,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_59_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,29,30,"AM"),calculate.calculate_time(new Time(6,59,30,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_60_30_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(6,60,30,"AM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_n1_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(6,30,-1,"AM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_0_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,0,0,"AM"),calculate.calculate_time(new Time(6,30,0,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_1_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,0,1,"AM"),calculate.calculate_time(new Time(6,30,1,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_58_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,0,58,"AM"),calculate.calculate_time(new Time(6,30,58,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_59_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,0,59,"AM"),calculate.calculate_time(new Time(6,30,59,"AM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_60_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(6,30,60,"AM"));
+                });
+    }
+
+
+    @Test
+    void calculateEndTimeOf_0_30_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(0,30,30,"PM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_1_30_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(3,0,30,"PM"),calculate.calculate_time(new Time(1,30,30,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_2_30_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(4,0,30,"PM"),calculate.calculate_time(new Time(2,30,30,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_11_30_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(1,0,30,"AM"),calculate.calculate_time(new Time(11,30,30,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_12_30_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(2,0,30,"PM"),calculate.calculate_time(new Time(12,30,30,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_13_30_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(13,30,30,"PM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_6_n1_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(6,-1,30,"PM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_6_0_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(7,30,30,"PM"),calculate.calculate_time(new Time(6,0,30,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_1_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(7,31,30,"PM"),calculate.calculate_time(new Time(6,1,30,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_58_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,28,30,"PM"),calculate.calculate_time(new Time(6,58,30,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_59_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,29,30,"PM"),calculate.calculate_time(new Time(6,59,30,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_60_30_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(6,60,30,"PM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_n1_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(6,30,-1,"PM"));
+                });
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_0_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,0,0,"PM"),calculate.calculate_time(new Time(6,30,0,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_1_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,0,1,"PM"),calculate.calculate_time(new Time(6,30,1,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_58_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,0,58,"PM"),calculate.calculate_time(new Time(6,30,58,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_59_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(8,0,59,"PM"),calculate.calculate_time(new Time(6,30,59,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_6_30_60_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(6,30,60,"PM"));
+                });
+    }
+
+    // Weak Equivalence Testing
+
+    @Test
+    void calculateEndTimeOf_2_15_46_AM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(3,45,46,"AM"),calculate.calculate_time(new Time(2,15,46,"AM")));
+    }
+    @Test
+    void calculateEndTimeOf_10_47_55_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(12,17,55,"AM"),calculate.calculate_time(new Time(10,47,55,"PM")));
+    }
+    @Test
+    void calculateEndTimeOf_11_33_15_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(1,3,15,"AM"),calculate.calculate_time(new Time(11,33,15,"PM")));
+    }
+    @Test
+    void calculateEndTimeOf_12_10_15_PM() {
+        CalculateTime calculate = new CalculateTime();
+        assertEquals(new Time(1,40,15,"PM"),calculate.calculate_time(new Time(12,10,15,"PM")));
+    }
+
+    @Test
+    void calculateEndTimeOf_13_66_n1_XM() {
+        CalculateTime calculate = new CalculateTime();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    calculate.calculate_time(new Time(13,66,-1,"XM"));
+                });
+    }
+}
